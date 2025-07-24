ccef9dde5ac84f865af5e52667cbc59cb50320a7
Kelar89 <kingshisha.indonesia@gmail.com>
2025-07-24 16:55:44 +0700

    Fix: Add pathPrefix for GitHub Pages deployment
    Fix: Add pathPrefix for GitHub Pages deployment

--- .eleventy.js
+++ .eleventy.js
@@ -3,15 +3,24 @@ module.exports = function(eleventyConfig) {
   // Aturan Passthrough Copy untuk folder assets
   eleventyConfig.addPassthroughCopy("assets");
 
-  // Filter 'dump' sudah dihapus karena tidak diperlukan lagi.
+  // Filter 'dump' untuk menanamkan data ke javascript
+  eleventyConfig.addFilter("dump", obj => {
+    return JSON.stringify(obj);
+  });
 
   return {
+    // Tentukan direktori
     dir: {
       input: ".",
       includes: "_includes",
       data: "_data",
       output: "_site"
     },
+    // PENTING: Tambahkan baris ini untuk memberitahu Eleventy
+    // bahwa situs berada di dalam sub-folder /rizkygadai/
+    pathPrefix: "/rizkygadai/",
+
+    // Template engine
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
   };
